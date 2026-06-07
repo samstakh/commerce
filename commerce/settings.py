53b8cd7526d4e3bccb5ed49780d418d2b59843b7
samstakh <samsithuc2@gmail.com>
2026-06-06 17:38:03 -0700

    Improved UI/UX

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['commerce-v32o.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'commerce-v32o.onrender.com']
 
 
 
